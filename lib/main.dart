e77e36b4482667144f1c3166db2639370edcc716
Andre Bastos <andrebastos1198@gmail.com>
2021-02-12 21:32:29 -0300

    app finalizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,6 +14,52 @@ void main() {
   ));
 }
 
+class LeftDice extends StatefulWidget {
+  @override
+  _LeftDiceState createState() => _LeftDiceState();
+}
+
+class _LeftDiceState extends State<LeftDice> {
+  int leftDiceNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            leftDiceNumber = Random().nextInt(6) + 1;
+          });
+        },
+        child: Image.asset('assets/dice$leftDiceNumber.png'),
+      ),
+    );
+  }
+}
+
+class RightDice extends StatefulWidget {
+  @override
+  _RightDiceState createState() => _RightDiceState();
+}
+
+class _RightDiceState extends State<RightDice> {
+  int rightDiceNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: FlatButton(
+        onPressed: () {
+          setState(() {
+            rightDiceNumber = Random().nextInt(6) + 1;
+          });
+        },
+        child: Image.asset('assets/dice$rightDiceNumber.png'),
+      ),
+    );
+  }
+}
+
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -20,18 +67,8 @@ class DicePage extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('assets/dice1.png'),
-            ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('assets/dice1.png'),
-            ),
-          ),
+          LeftDice(),
+          RightDice(),
         ],
       ),
     );
